a6c340e58f31ceb075042f7fc8d7e0abb4c4c7d1
ArjunB126 <arjunbajare86@gmail.com>
2024-04-10 10:18:20 +0530

    remove tour function declaration in cards component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,35 @@
 import { useState } from "react";
 
-function Card(id,name,info,price,image) {
+function Card(id, name, info, price, image) {
+  const [readmore, setReadmore] = useState(false);
 
-    const[readmore ,setReadmore] = useState(false);
+  const description = `${info.subString(0, 200)}....`;
 
-    const description = `${info.subString(0,200)}....`
+  function readmoreHandler() {
+    setReadmore(!readmore);
+  }
 
-    function readmoreHandler(){
-        setReadmore(!readmore);
-    }
-
-
-    return(
-        <div className="card">
-            <img src={image} className="image"></img>
-
-            <div className="tour-details">
-                <h4 className="tour-price">{price}</h4>
-                <h4 className="tour-name">{name}</h4>
-            </div>
-
-            <div className="description">
-                {description}
-                <span className="read-more" onClick={readmoreHandler}>
-                    {readmore ?`show less`: ` Read more`} 
-                </span>
-            </div>
+  return (
+    <div className="card">
+      <img src={image} className="image"></img>
+      <div>
+        <div className="tour-details">
+          <h4 className="tour-price">{price}</h4>
+          <h4 className="tour-name">{name}</h4>
         </div>
-    )
+
+        <div className="description">
+          {description}
+          <span className="read-more" onClick={readmoreHandler}>
+            {readmore ? `show less` : ` Read more`}
+          </span>
+        </div>
+      </div>
+      <button className="btn-red" onClick={removeTour}>
+         Not Intrested 
+      </button>
+    </div>
+  );
 }
 
-export default  Card;
+export default Card;
